e65ea085f26631d157e10a01a47dcb2159e6a956
Greg Frady <greg.frady@gmail.com>
2016-06-26 22:40:31 -0400

    Adding status codes enum

---
+++ src/main/java/com/binaryapothecary/common_utils/web/HttpStatusCode.java
@@ -0,0 +1,127 @@
+package com.binaryapothecary.common_utils.web;
+
+/**
+ * Enumeration of Http status codes. This should be in Java somewhere by default by now.
+ *
+ * See:
+ * https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+ *
+ * Created by grefrady on 2/29/16.
+ */
+public enum HttpStatusCode {
+    UNKNOWN(-1, "Unknown"),
+    
+    // 100s
+    CONTINUE(100, "Continue"),
+    SWITCHING_PROTOCOLS(101, "Switching Protocols"),
+    PROCESSING(102, "Processing"),
+
+    // 200s
+    OK(200, "OK"),
+    CREATED(201, "Created"),
+    ACCEPTED(202, "Accepted"),
+    NON_AUTHORITATIVE_INFORMATION(203, "Non-Authoritative Information"),
+    NO_CONTENT(204, "No Content"),
+    RESET_CONTENT(205, "Reset Content"),
+    PARTIAL_CONTENT(206, "Partial Content"),
+    MULTI_STATUS(207, "Multi-Status"),
+    ALREADY_REPORTED(208, "Already Reported"),
+    IM_USED(226, "IM Used"),
+
+    // 300s
+    MULTIPLE_CHOICES(300, "Multiple Choices"),
+    MOVED_PERMANENTLY(301, "Moved Permanently"),
+    FOUND(302, "Found"),
+    SEE_OTHER(303, "See Other"),
+    NOT_MODIFIED(304, "Not Modified"),
+    USE_PROXY(305, "Use Proxy"),
+    SWITCH_PROXY(306, "Switch Proxy"),
+    TEMPORARY_REDIRECT(307, "Temporary Redirect"),
+    PERMANENT_REDIRECT(308, "Permanent Redirect"),
+
+    // 400s
+    BAD_REQUEST(400, "Bad Request"),
+    UNAUTHORIZED(401, "Unauthorized"),
+    PAYMENT_REQUIRED(402, "Payment Required"),
+    FORBIDDEN(403, "Forbidden"),
+    NOT_FOUND(404, "Not Found"),
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+    NOT_ACCEPTABLE(406, "Not Acceptable"),
+    PROXY_AUTHENTICATION_REQUIRED(407, "Proxy Authentication Required"),
+    REQUEST_TIMEOUT(408, "Request Timeout"),
+    CONFLICT(409, "Conflict"),
+    GONE(410, "Gone"),
+    LENGTH_REQUIRED(411, "Length Required"),
+    PRECONDITION_FAILED(412, "Precondition Failed"),
+    PAYLOAD_TOO_LARGE(413, "Payload Too Large"),
+    URI_TOO_LONG(414, "URI Too Long"),
+    UNSUPPORTED_MEDIA_TYPE(415, "Unsupported Media Type"),
+    RANGE_NOT_SATISFIABLE(416, "Range Not Satisfiable"),
+    EXPECTATION_FAILED(417, "Expectation Failed"),
+    IM_A_TEAPOT(418, "I'm a teapot"),
+    TEAPOT(418, "I'm a teapot"),
+    MISDIRECTED_REQUEST(421, "Misdirected Request"),
+    UNPROCESSABLE_ENTITY(422, "Unprocessable Entity"),
+    LOCKED(423, "Locked"),
+    FAILED_DEPENDENCY(424, "Failed Dependency"),
+    UPGRADE_REQUIRED(426, "Upgrade Required"),
+    PRECONDITION_REQUIRED(428, "Precondition Required"),
+    TOO_MANY_REQUESTS(429, "Too Many Requests"),
+    REQUEST_HEADER_FIELDS_TOO_LARGE(431, "Request Header Fields Too Large"),
+    UNAVAILABLE_FOR_LEGAL_REASONS(451, "Unavailable For Legal Reasons"),
+    FAHRENHEIT_451(451, "Unavailable For Legal Reasons"),
+
+    // 500s
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+    SERVER_ERROR(500, "Internal Server Error"),
+    NOT_IMPLEMENTED(501, "Not Implemented"),
+    BAD_GATEWAY(502, "Bad Gateway"),
+    SERVICE_UNAVAILABLE(503, "Service Unavailable"),
+    GATEWAY_TIMEOUT(504, "Gateway Timeout"),
+    HTTP_VERSION_NOT_SUPPORTED(505, "HTTP Version Not Supported"),
+    VARIANT_ALSO_NEGOTIATES(506, "Variant Also Negotiates"),
+    INSUFFICIENT_STORAGE(507, "Insufficient Storage"),
+    LOOP_DETECTED(508, "Loop Detected"),
+    NOT_EXTENDED(510, "Not Extended"),
+    NETWORK_AUTHENTICATION_REQUIRED(511, "Network Authentication Required"),
+
+    // Unofficial
+    TWITTER_ENHANCE_YOUR_CALM(420, "Enhance Your Calm")
+    ;
+
+    private int code;
+    private String name;
+
+    HttpStatusCode(int code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public String getCodeString() {
+        return Integer.toString(this.code);
+    }
+
+    public int getCode() {
+        return this.code;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * Retrieve the HttpStatusCode representation of an integer.
+     *
+     * @param responseCodeAsInt the integer representation
+     * @return the representing HttpStatusCode
+     */
+    public static HttpStatusCode getStatusCode(int responseCodeAsInt) {
+        for (HttpStatusCode statusCode : HttpStatusCode.values()) {
+            if (statusCode.getCode() == responseCodeAsInt) {
+                return statusCode;
+            }
+        }
+
+        return HttpStatusCode.UNKNOWN;
+    }
+}
